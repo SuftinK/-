5177e6778e1fe03d00a02783e20565f8282ea1f4
SufK <sufk@rambler.ru>
2022-11-15 17:09:06 +0300

    class preparation v2.2

--- Classes.py
+++ Classes.py
@@ -2,7 +2,7 @@ class Tablo:
     def __init__(self, word : str) -> None:
         self._word = word.upper()
         self._letter = ""
-        self._hidden = "*" * len(self._word)
+        self._hidden = list("*" * len(self._word))
         let_dict = dict()
         word_copy = list(self._word)[:]
         for i in range(0, len(self._word)):
@@ -10,20 +10,18 @@ class Tablo:
             let_dict[word_copy[i]] = let_dict.get(word_copy[i], list())
             let_dict[word_copy[i]].append(i)
         print(let_dict)
+        self._let_dict = let_dict
 
     def __str__(self):
         return f'{self._word} The hidden word is: {self._hidden}, the letter is {self._letter}'
 
-    # def letters_and_indexs(self):
-    #
-    #     let_dict = dict()
-    #     word_copy = list(self._word)[:]
-    #     for i in range(0, len(self._word)):
-    #         print(i, word_copy[i])
-    #         let_dict[word_copy[i]] = let_dict.get(word_copy[i], list())
-    #         let_dict[word_copy[i]].append(i)
-    #
-    #         print(let_dict)
+    def open_the_new_letters(self):
+        if self._letter in self._word:
+            print(f"Congratulation! This letter {self._letter} presented in hidden word")
+            for i in self._let_dict[self._letter]:
+                self._hidden[i] = self._letter
+        else:
+            print(f"Sorry! The letter {self._letter} missed in hidden word")
 
     @property
     def word(self) -> str:
@@ -40,14 +38,25 @@ class Tablo:
     @letter.setter
     def letter(self, l) -> None:
         if isinstance(l, str) and l.isalpha() and len(l) == 1:
-            self._letter = l
+            self._letter = l.upper()
         else:
             raise TypeError('The letter should be typed in letters and lenght should be 1 ch ')
 
 T = Tablo('hello')
 print(T)
-T2 = Tablo('Carshearing')
+T2 = Tablo('hello')
 print(T2)
-# Tablo.letter = "h"
+T2.letter = "e"
+print(T2)
+T2.open_the_new_letters()
+print(T2)
+T2.letter = "l"
+print(T2)
+T2.open_the_new_letters()
+print(T2)
+T2.letter = "h"
+print(T2)
+T2.open_the_new_letters()
+print(T2)
 # Tablo.word = "red"
 # Tablo.letters_and_indexs()
